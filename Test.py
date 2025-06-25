c5dbd51ce4d417bb613dc790d73350ee2d4f7ec7
manish <60966343+mourya88@users.noreply.github.com>
2025-06-25 14:53:58 +0100

    Update Test.py

--- Test.py
+++ Test.py
@@ -1,9 +1,1 @@
-def parameters_changed(existing_params, new_params):
-    # Convert both lists to dictionaries using correct key names
-    existing_dict = {p["ParameterKey"]: p.get("DefaultValue", "") for p in existing_params}
-    new_dict = {p["Key"]: p["Value"] for p in new_params}
 
-    print("Existing:", existing_dict)
-    print("New:", new_dict)
-
-    return existing_dict != new_dict
