10a4aacb124130e50a9c29e33a63ec5b605754f5
manish <60966343+mourya88@users.noreply.github.com>
2025-07-06 22:25:18 +0100

    Update Test.py

--- Test.py
+++ Test.py
@@ -1,5 +1,1 @@
-parameters = stack['Stacks'][0].get('Parameters', [])
-print(f"Raw Parameters from CFN DescribeStacks: {parameters}")
-
-if not parameters:
-    print("❗No parameters found in CFN stack — check if they were defined in template.")
+print(f"✅ Current Parameters (from CFN): {json.dumps(current_params, indent=2)}")
