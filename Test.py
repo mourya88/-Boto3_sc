9875f6d19e16212b43707466b8de2caf280c878a
manish <60966343+mourya88@users.noreply.github.com>
2025-07-06 22:31:12 +0100

    Update Test.py

--- Test.py
+++ Test.py
@@ -1,1 +1,11 @@
-print(f"✅ Current Parameters (from CFN): {json.dumps(current_params, indent=2)}")
+if not parameters:
+    print("❗ No parameters found in CFN stack – check if they were defined in template.")
+
+# Convert CFN parameters to same format
+current_params = [
+    {"ParameterKey": p["ParameterKey"], "DefaultValue": p["ParameterValue"]}
+    for p in parameters
+]
+print(f"🟢 Current Parameters (from CFN): {json.dumps(current_params, indent=2)}")
+
+return current_params
