560a5c61820937290c4beba95a3cb19b2cc09b6d
manish <60966343+mourya88@users.noreply.github.com>
2025-07-29 15:21:04 +0100

    Update Test.py

--- Test.py
+++ Test.py
@@ -1,1 +1,12 @@
-]
+]{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "AWS": "arn:aws:iam::<source-account-id>:role/<source-role-name>"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
